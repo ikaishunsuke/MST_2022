7538a866be74eebdfa7a1cf66de4f909bd5dbfbf
佐々木美咲 <the.glass007@icloud.com>
2021-10-14 14:48:30 +0900

    にゅんにゅん

--- MST_2022/Assets/Script/System/CMedicalRecord.cs
+++ MST_2022/Assets/Script/System/CMedicalRecord.cs
@@ -26,11 +26,14 @@ public class CMedicalRecord : MonoBehaviour
     private GameObject _gPanel;
     public static GameObject _gNewMark; // まだカルテ詳細をみていないやつがある場合は必ずつく。
     public GameObject[] _gHint;
+    private GameObject _gPausePanel;
 
     /* PlayerPrefsの関係上boolにしたかったけどintで我慢します。
        int = 0の時がfalse,int = 1の時がtrueとする。   */
     public static int[] _iHint = new int[8];
 
+    private GameObject mainCamera;      // メインカメラ格納用
+    private GameObject subCamera;       // サブカメラ格納用 
 
     // Start is called before the first frame update
     void Start()
@@ -60,43 +63,70 @@ public class CMedicalRecord : MonoBehaviour
         _gNewMark.SetActive(false);
         _gPanel = GameObject.Find("Panel_MedicalRecord");
         _gPanel.SetActive(false);
+
+        _gPausePanel = GameObject.Find("Panel_Pause");
+
+        //メインカメラとサブカメラをそれぞれ取得
+        mainCamera = GameObject.Find("MainCamera");
+        subCamera = GameObject.Find("MedicalRecord_Camera");
+
+        //サブカメラを非アクティブにする
+        subCamera.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (CInputManager.GetButtonDown(INPUT_CODE.X))
+        if (!_gPausePanel.activeSelf)
         {
-            if (!_gPanel.activeSelf)
+            if (CInputManager.GetButtonDown(INPUT_CODE.X))
             {
-                _gNewMark.SetActive(false);
-                _gIcon.SetActive(false);
-                _gPanel.SetActive(true);
-
-
-                // カルテデータの更新
-                //for (int index = 0; index < _iHint.Length; index++)
-                for (int index = 0; index < 2; index++)
+                if (!_gPanel.activeSelf)
                 {
-                    // カルテデータが開示されていない場合
-                    if (_iHint[index] == 0)
+                    _gNewMark.SetActive(false);
+                    _gIcon.SetActive(false);
+                    _gPanel.SetActive(true);
+
+
+                    // カルテデータの更新
+                    //for (int index = 0; index < _iHint.Length; index++)
+                    for (int index = 0; index < 2; index++)
                     {
-                        _gHint[index * 2].SetActive(true);
-                        _gHint[1 + index * 2].SetActive(false);
-                    }
-                    // カルテデータが開示されている場合
-                    if (_iHint[index] == 1)
-                    {
-                        _gHint[index * 2].SetActive(false);
-                        _gHint[1 + index * 2].SetActive(true);
+                        // カルテデータが開示されていない場合
+                        if (_iHint[index] == 0)
+                        {
+                            _gHint[index * 2].SetActive(true);
+                            _gHint[1 + index * 2].SetActive(false);
+                        }
+                        // カルテデータが開示されている場合
+                        if (_iHint[index] == 1)
+                        {
+                            _gHint[index * 2].SetActive(false);
+                            _gHint[1 + index * 2].SetActive(true);
+                        }
                     }
                 }
+                else
+                {
+                    _gPanel.SetActive(false);
+                    _gIcon.SetActive(true);
+                }
+            }
+
+            // カルテが表示されている場合
+            if (_gPanel.activeSelf)
+            {
+                //サブカメラをアクティブに設定
+                mainCamera.SetActive(false);
+                subCamera.SetActive(true);
             }
             else
             {
-                _gPanel.SetActive(false);
-                _gIcon.SetActive(true);
+                //メインカメラをアクティブに設定
+                subCamera.SetActive(false);
+                mainCamera.SetActive(true);
             }
+
         }
     }
 
